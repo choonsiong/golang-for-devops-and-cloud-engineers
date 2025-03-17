72b4f48c869d5996b00f5e0470d2b10eed4718db
choonsiong <choonsiong.lee@gmail.com>
2025-03-17 21:35:07 +0800

    Auto commit

--- introduceinterface/main.go
+++ introduceinterface/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 )
 
 type SlowReader struct {
@@ -13,10 +14,14 @@ type SlowReader struct {
 
 func (s *SlowReader) Read(p []byte) (n int, err error) {
 	if s.pos < len(s.content) {
+		log.Printf("Reading '%s' and sleep for a while...", string(s.content[s.pos]))
+		time.Sleep(time.Second * 1)
 		n = copy(p, s.content[s.pos:s.pos+1])
 		s.pos += 1
 		return n, nil
 	}
+
+	log.Println("We are done reading 完成了，ありがとうございました 💁🏿💁🏿💁🏿...")
 
 	return 0, io.EOF
 }
